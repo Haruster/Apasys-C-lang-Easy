708f67d4f7d22e518f851fbff3cb2f1c9bd28804
Apasys <67867993+Apasys@users.noreply.github.com>
2021-10-08 13:14:00 +0900

    Create 변수 선언과 동시에 초기화.c

---
+++ clang practice/변수 선언과 동시에 초기화.c
@@ -0,0 +1,14 @@
+//변수 선언과 동시에 초기화
+#include <stdio.h>
+
+int main(void) {
+    
+    int num1 = 10;
+    int num2 = 20;
+    
+    printf("num1 = %d \n", num1);
+    printf("num2 = %d \n", num2);
+    
+    return 0;
+    
+}
