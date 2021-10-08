9d1acd54fae9a0013a458b7abda9e80ad1e0e487
Apasys <67867993+Apasys@users.noreply.github.com>
2021-10-08 13:11:08 +0900

    Create 변수 선 선언 후 초기화.c

---
+++ clang practice/변수 선 선언 후 초기화.c
@@ -0,0 +1,17 @@
+//변수 선 선언 후 초기화
+
+#include <stdio.h>
+
+int main(void) {
+    
+    int a;
+    int b;
+    
+    a = 10;
+    b = 20;
+    
+    printf("a는 %d이고, b는 %d입니다. \n", a, b);
+    
+    return 0;
+    
+}
