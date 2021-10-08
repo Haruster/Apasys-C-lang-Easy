b86a87eb49e7bbe68dbd563cbc21b306ce3c0855
Apasys <67867993+Apasys@users.noreply.github.com>
2021-10-08 15:39:45 +0900

    Create double을 이용하여 실수 출력.c

---
+++ clang practice/double을 이용하여 실수 출력.c
@@ -0,0 +1,14 @@
+//double을 이용해서 실수 출력
+
+#include <stdio.h>
+
+int main(void) {
+    
+    double num1 = 3.5453543;
+    double num2 = 1.4343555;
+    
+    printf("num1의 값은 %lf이고 num2의 값은 %lf입니다.\n", num1, num2); //double은 %lf를 이용해서 출력한다.
+    
+    return 0;
+    
+}
