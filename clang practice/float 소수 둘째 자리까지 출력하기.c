5cb5f30690949d43c848587164008669010d3c37
Apasys <67867993+Apasys@users.noreply.github.com>
2021-10-08 15:58:49 +0900

    Create float 소수 둘째 자리까지 출력하기.c

---
+++ clang practice/float 소수 둘째 자리까지 출력하기.c
@@ -0,0 +1,13 @@
+//float 소수 둘째 자리까지 출력하기
+
+#include <stdio.h>
+
+int main(void) {
+    
+    float data = 3.141592;
+    
+    printf("data의 값은 %.2f입니다.", data); //%.2f : 소수점 둘째 자리까지 출력한다.
+    
+    return 0;
+    
+}
