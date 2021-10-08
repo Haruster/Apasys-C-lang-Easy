040e5bcdd4187bfe51d0b4ce244fa1a3a245c741
Apasys <67867993+Apasys@users.noreply.github.com>
2021-10-08 15:37:11 +0900

    Update float을 이용해서 실수 출력.c

--- clang practice/float을 이용해서 실수 출력.c
+++ clang practice/float을 이용해서 실수 출력.c
@@ -7,6 +7,6 @@ int main(void) {
     float num1 = 3.1233;
     float num2 = 3.25;
     
-    printf("num1은 %f입니다. \n "); //float은 %f를 이용해서 출력한다.
+    printf("num1은 %f이고 num2sms %f입니다. \n "); //float은 %f를 이용해서 출력한다.
     
 }
